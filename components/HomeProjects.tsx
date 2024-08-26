fa040a329d192d9369ceb203951f75d6090d7caf
Jason A Savage <32180237+TheSavageDev@users.noreply.github.com>
2024-08-25 21:32:21 -0500

    updates

--- components/HomeProjects.tsx
+++ components/HomeProjects.tsx
@@ -9,29 +9,29 @@ const HomeProjects = () => {
           <hr />
         </header>
         <section className="flex flex-wrap justify-between md:flex-nowrap">
-          <article className="xl:w-1/4 md:w-1/2 p-4 ">
+          <article className="xl:w-1/2 md:w-1/2 p-4 ">
             <article className="bg-primary p-6 rounded-xl shadow-lg hover:shadow-2xl">
               <a
-                href="https://shoptrendysavages.com"
+                href="https://www.ascensionkofc.com/"
                 target="_blank"
                 rel="noopener noreferrer nofollow"
               >
                 <img
-                  className="h-40 rounded-xl w-full object-cover object-center mb-6"
-                  src="/images/ts.png"
-                  alt="Trendy Savages"
-                  title="Trendy Savages"
+                  className="h-40 rounded-xl w-full object-contain object-center mb-6"
+                  src="/images/kofc.png"
+                  alt="Ascension Knights of Columbus"
+                  title="Ascension Knights of Columbus"
                 />
               </a>
               <h3 className="tracking-widest text-yellow text-xs font-medium title-font uppercase">
-                Shopify Website
+                WordPress Website
               </h3>
               <h2 className="text-lg text-gray-100 font-medium title-font mb-4">
-                Trendy Savages
+                Ascension KofC
               </h2>
             </article>
           </article>
-          <article className="xl:w-1/4 md:w-1/2 p-4">
+          <article className="xl:w-1/2 md:w-1/2 p-4">
             <article className="bg-primary p-6 rounded-xl shadow-lg hover:shadow-2xl">
               <a
                 href="https://www.orthoathleticedu.com/"
@@ -39,7 +39,7 @@ const HomeProjects = () => {
                 rel="noopener noreferrer nofollow"
               >
                 <img
-                  className="h-40 rounded-xl w-full object-cover object-center mb-6"
+                  className="h-40 rounded-xl w-full object-contain object-center mb-6"
                   src="/images/orthoathletic.png"
                   alt="OrthoAthletic Education, LLC"
                   title="OrthoAthletic Education, LLC"
@@ -53,25 +53,101 @@ const HomeProjects = () => {
               </h2>
             </article>
           </article>
-          <article className="xl:w-1/4 md:w-1/2 p-4">
+          <article className="xl:w-1/2 md:w-1/2 p-4">
             <article className="bg-primary p-6 rounded-xl shadow-lg hover:shadow-2xl">
               <a
-                href="https://foldawaytrailer.com/"
+                href="https://rndkc.vercel.app"
                 target="_blank"
                 rel="noopener noreferrer nofollow"
               >
                 <img
-                  className="h-40 rounded-xl w-full object-cover object-center mb-6"
-                  src="/images/ftc.png"
-                  alt="Foldaway Trailer Company"
-                  title="Foldaway Trailer Company"
+                  className="h-40 rounded-xl w-full object-contain object-center mb-6"
+                  src="/images/rnd.svg"
+                  alt="RND Garage"
+                  title="RND Garage"
                 />
               </a>
               <h3 className="tracking-widest text-yellow text-xs font-medium title-font uppercase">
-                Gatsby Website
+                NextJS Website
               </h3>
               <h2 className="text-lg text-gray-100 font-medium title-font mb-4">
-                Foldaway Trailer Company
+                RND Garage
+              </h2>
+            </article>
+          </article>
+          <article className="xl:w-1/2 md:w-1/2 p-4">
+            <article className="bg-primary p-6 rounded-xl shadow-lg hover:shadow-2xl">
+              <a
+                href="https://www.ks3130.com"
+                target="_blank"
+                rel="noopener noreferrer nofollow"
+              >
+                <img
+                  className="h-40 rounded-xl w-full object-contain object-center mb-6"
+                  src="/images/ahg.jpg"
+                  alt="American Heritage Girls Troop KS-3130"
+                  title="American Heritage Girls Troop KS-3130"
+                />
+              </a>
+              <h3 className="tracking-widest text-yellow text-xs font-medium title-font uppercase">
+                NextJS Website
+              </h3>
+              <h2 className="text-lg text-gray-100 font-medium title-font mb-4">
+                AHG Troop KS-3130
+              </h2>
+            </article>
+          </article>
+        </section>
+      </section>
+      <section className="p-5 max-h-full my-5 w-full rounded-xl transition-all duration-200 md:w-3/4 md:mx-auto lg:w-9/12">
+        <header className="lg:w-1/2 w-full mb-6 xl:mb-6 lg:mb-0">
+          <h2 className="sm:text-3xl text-2xl font-medium title-font text-gray-100">
+            Works in Progress
+          </h2>
+          <hr />
+        </header>
+        <section className="flex flex-wrap justify-between md:flex-nowrap">
+          <article className="xl:w-1/2 md:w-1/2 p-4 ">
+            <article className="bg-primary p-6 rounded-xl shadow-lg hover:shadow-2xl">
+              <a
+                href="https://mpp-seven.vercel.app"
+                target="_blank"
+                rel="noopener noreferrer nofollow"
+              >
+                <img
+                  className="h-40 rounded-xl w-full object-contain object-center mb-6"
+                  src="/images/d4v.webp"
+                  alt="Dogs 4 Valor"
+                  title="Dogs 4 Valor"
+                />
+              </a>
+              <h3 className="tracking-widest text-yellow text-xs font-medium title-font uppercase">
+                NextJS Website
+              </h3>
+              <h2 className="text-lg text-gray-100 font-medium title-font mb-4">
+                Dogs 4 Valor
+              </h2>
+            </article>
+          </article>
+          <article className="xl:w-1/2 md:w-1/2 p-4">
+            <article className="bg-primary p-6 rounded-xl shadow-lg hover:shadow-2xl">
+              <a
+                href="https://savagenails.info"
+                target="_blank"
+                rel="noopener noreferrer nofollow"
+              >
+                <img
+                  className="h-40 rounded-xl w-full object-contain object-center mb-6"
+                  src="/images/vercel.svg"
+                  alt="RND Garage"
+                  title="RND Garage"
+                />
+              </a>
+              <h3 className="tracking-widest text-yellow text-xs font-medium title-font uppercase">
+                NextJS Website
+              </h3>
+              <h2 className="text-lg text-gray-100 font-medium title-font mb-4">
+                Beauty & Grace with the Savage Gals
               </h2>
             </article>
           </article>
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,38 +11,10 @@ const Home: NextPage = () => {
         <title>TheSavageDev</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <header className="flex bg-translucent text-white justify-between fixed right-0 top-0 left-0 z-10">
+      <header className="flex bg-black text-white justify-between z-10">
         <h1 className="text-5xl text-yellow mx-5">JS</h1>
       </header>
       <main className="w-full m-0 bg-gray-900 text-white" id="home-main">
-        <section
-          id="home"
-          className="relative bg-center bg-no-repeat bg-cover bg-cross w-screen h-screen"
-        >
-          <section className="bg-overlay w-full h-full flex flex-col justify-center items-center">
-            <header>
-              <h2 id="title" className="text-4xl font-semibold text-white">
-                I am Jason A Savage
-              </h2>
-            </header>
-            <section className="flex flex-wrap justify-between">
-              <article className="jobs">
-                <span id="catholic" className="text-lg mx-2">
-                  Catholic
-                </span>
-                <span id="father" className="text-lg mx-2">
-                  Father
-                </span>
-                <span id="husband" className="text-lg mx-2">
-                  Husband
-                </span>
-                <span id="se" className="text-lg mx-2">
-                  Software Engineer
-                </span>
-              </article>
-            </section>
-          </section>
-        </section>
         <HomeProjects />
         <HomeServices />
       </main>
